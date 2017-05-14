3e75751aea1b19d9f7ba6660103203f3d36decef
unknown <salilthip.p@ku.th>
2017-05-14 16:16:10 +0700

    Can get location every 5 seconds

--- app/src/main/java/com/example/user/loginfirebase/ViewLocation.java
+++ app/src/main/java/com/example/user/loginfirebase/ViewLocation.java
@@ -1,22 +1,30 @@
 package com.example.user.loginfirebase;
 
 import android.Manifest;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class ViewLocation extends AppCompatActivity {
 
-    private Button getLocation;
+    private Button getLocation,stopGPS;
+    private TextView timerCounting;
     private static final int REQUEST_CODE_PERMISSION = 2;
     String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
     private int counter = 1;
     GPSTracker gps;
+
+    Handler h = new Handler();
+    Thread task;
+    private long startTime;
+    private String timeString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,22 +36,69 @@ public class ViewLocation extends AppCompatActivity {
                ActivityCompat.requestPermissions(this,new String[]{mPermission},REQUEST_CODE_PERMISSION);
            }
         }catch (Exception e){
-
+            e.printStackTrace();
         }
         getLocation = (Button)findViewById(R.id.getLocationBtn);
+        stopGPS = (Button)findViewById(R.id.stopGPSBtn);
+        timerCounting = (TextView)findViewById(R.id.timerCounting);
+        stopGPS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopHandlerTask();
+                timerCounting.setText("Location Service is Stopped.");
+            }
+        });
         getLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gps = new GPSTracker(ViewLocation.this);
-                if(gps.canGetLocation()){
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
-                    Toast.makeText(getApplicationContext(),counter+"\nCurrent location is \nLat:"+latitude+"\nLong:"+longitude,Toast.LENGTH_SHORT).show();
-                }else{
-                    gps.showSettingsAlert();
-                }
-                counter++;
+                startTimer();
+                counter = 1;
+
             }
         });
+
+
+
     }
+
+    public void showToastCurrentLocation(){
+        gps = new GPSTracker(ViewLocation.this);
+        if(gps.canGetLocation()){
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+            Toast.makeText(getApplicationContext(),"Current location is \nLat:"+latitude+"\nLong:"+longitude,Toast.LENGTH_SHORT).show();
+        }else{
+            gps.showSettingsAlert();
+        }
+    }
+
+    public void startTimer() {
+        startTime = System.currentTimeMillis();
+        task  = new Thread(){
+            @Override
+            public void run() {
+                long millis = System.currentTimeMillis() - startTime;
+                long secs = millis /1000 % 60; // second
+    ////////////////////////////
+                timeString = String.format("%02d",secs); //2digit
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        timerCounting.setText(timeString);
+                    }
+                });
+                h.postDelayed(task,1000); //upadate every 1 second
+                if(secs%5==0) showToastCurrentLocation();
+//                    super.run();
+            }
+        };
+        ///////////////////////
+        h.postDelayed(task,1000);
+    }
+
+    public void stopHandlerTask(){
+        h.removeCallbacks(task);
+    }
+
+
 }
