aafdb61e9f309ed52eb1b228a0c8c255a954bf33
unknown <salilthip.p@ku.th>
2017-05-14 16:59:56 +0700

    Store the gps location in Database

--- app/src/main/java/com/example/user/loginfirebase/ViewLocation.java
+++ app/src/main/java/com/example/user/loginfirebase/ViewLocation.java
@@ -2,6 +2,7 @@ package com.example.user.loginfirebase;
 
 import android.Manifest;
 import android.os.Handler;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,10 +11,17 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.client.Firebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class ViewLocation extends AppCompatActivity {
 
+    private DatabaseReference mRootRef;
     private Button getLocation,stopGPS;
     private TextView timerCounting;
     private static final int REQUEST_CODE_PERMISSION = 2;
@@ -41,6 +49,9 @@ public class ViewLocation extends AppCompatActivity {
         getLocation = (Button)findViewById(R.id.getLocationBtn);
         stopGPS = (Button)findViewById(R.id.stopGPSBtn);
         timerCounting = (TextView)findViewById(R.id.timerCounting);
+
+        mRootRef = FirebaseDatabase.getInstance().getReference("GPS_Location").push();
+
         stopGPS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,6 +77,9 @@ public class ViewLocation extends AppCompatActivity {
         if(gps.canGetLocation()){
             double latitude = gps.getLatitude();
             double longitude = gps.getLongitude();
+            mRootRef.child("currentTIme :").setValue(System.currentTimeMillis()+"");
+            mRootRef.child("latitude").setValue(latitude+"");
+            mRootRef.child("longitude").setValue(longitude+"");
             Toast.makeText(getApplicationContext(),"Current location is \nLat:"+latitude+"\nLong:"+longitude,Toast.LENGTH_SHORT).show();
         }else{
             gps.showSettingsAlert();
